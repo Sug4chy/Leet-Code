a46cbb304269db0d7e0b540b8c2c9b405420ac36
Степан <sb.9226981433@gmail.com>
2024-03-11 22:06:37 +0500

    1768

---
+++ LeetCode/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(new Solution().MergeAlternately("abcd", "pq"));
+    }
+}
---
+++ LeetCode/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace LeetCode;
+
+public class Solution
+{
+    //1768
+    public string MergeAlternately(string word1, string word2)
+    {
+        int i = 0;
+        int j = 0;
+        var sb = new StringBuilder();
+        bool flag = false;
+        while (i < word1.Length && j < word2.Length)
+        {
+            if (flag)
+            {
+                sb.Append(word2[j]);
+                j++;
+            }
+            else
+            {
+                sb.Append(word1[i]);
+                i++;
+            }
+
+            flag = !flag;
+        }
+
+        sb.Append(i == word1.Length ? word2[j..] : word1[i..]);
+        return sb.ToString();
+    }
+}
