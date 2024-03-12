a9b7c41aaf86b946ca6e8d029d8aab5cea0c9494
Степан <sb.9226981433@gmail.com>
2024-03-12 08:47:11 +0500

    .

---
+++ LeetCode/Helpers/ListNode.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Helpers;
+
+public class ListNode(int val = 0, ListNode next = null)
+{
+    public int val = val;
+    public ListNode next = next;
+}
+
+public static class ListNodeExtensions
+{
+    public static List<int> ToList(this ListNode head)
+    {
+        var current = head;
+        var result = new List<int>();
+        while (current is not null)
+        {
+            result.Add(current.val);
+            current = current.next;
+        }
+
+        return result;
+    }
+}
