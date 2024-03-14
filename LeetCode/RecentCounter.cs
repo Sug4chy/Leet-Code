ae9498fb59b14498efdaa8f4c8ecae1f78f10aa1
Степан <sb.9226981433@gmail.com>
2024-03-14 16:28:48 +0500

    933

---
+++ LeetCode/RecentCounter.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode;
+
+//933
+public class RecentCounter
+{
+    private readonly Queue<int> _requests;
+    
+    public RecentCounter()
+    {
+        _requests = new Queue<int>();
+    }
+
+    public int Ping(int t)
+    {
+        _requests.Enqueue(t);
+        while (_requests.Peek() < t - 3000)
+        {
+            _requests.Dequeue();
+        }
+
+        return _requests.Count;
+    }
+}
